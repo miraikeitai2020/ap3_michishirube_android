b7916d444bd8b03796b32aab47565cc80e6b1a3b
nanairoaisu <b1018078@fun.ac.jp>
2020-09-29 06:26:05 +0900

    feat:SpotListViewModelの追加

---
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/spotList/SpotListViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.michishirube.ui.spotList
+
+import androidx.lifecycle.ViewModel
+
+class SpotListViewModel: ViewModel() {
+    private lateinit var spotListViewModel: SpotListViewModel by viewModels()
+
+}
