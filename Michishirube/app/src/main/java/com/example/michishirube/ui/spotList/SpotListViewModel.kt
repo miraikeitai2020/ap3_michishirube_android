614f8560e2d1893de95f7fb51e8e66d38d860099
nanairoaisu <b1018078@fun.ac.jp>
2020-09-29 06:41:53 +0900

    chore:ViewModelのをgradleに追加

--- Michishirube/app/src/main/java/com/example/michishirube/ui/spotList/SpotListViewModel.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/spotList/SpotListViewModel.kt
@@ -3,6 +3,5 @@ package com.example.michishirube.ui.spotList
 import androidx.lifecycle.ViewModel
 
 class SpotListViewModel: ViewModel() {
-    private lateinit var spotListViewModel: SpotListViewModel by viewModels()
 
 }
