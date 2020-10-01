27bf1916139bbade797cf220c29e8f29f2e4c2a1
nanairoaisu <b1018078@fun.ac.jp>
2020-10-01 22:24:07 +0900

    style:layoutの文字サイズ統一

--- Michishirube/app/src/main/java/com/example/michishirube/ui/title/TitleFragment.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/title/TitleFragment.kt
@@ -1,17 +1,40 @@
 package com.example.michishirube.ui.title
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.michishirube.R
 import kotlinx.android.synthetic.main.fragment_title.view.*
+import java.util.jar.Manifest
 
 class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_title, container, false)
+
+//        val requestPermissionLauncher =
+//            registerForActivityResult(RequestPermission()
+//            ) { isGranted: Boolean ->
+//                if (isGranted) {
+//                    // Permission is granted. Continue the action or workflow in your
+//                    // app.
+//                } else {
+//                    // Explain to the user that the feature is unavailable because the
+//                    // features requires a permission that the user has denied. At the
+//                    // same time, respect the user's decision. Don't link to system
+//                    // settings in an effort to convince the user to change their
+//                    // decision.
+//                }
+//            }
+
+        //押されたら
         view.ibNavigation.setOnClickListener {
             findNavController().navigate(R.id.action_title_to_naviEmotionSelect)
         }
@@ -19,6 +42,23 @@ class TitleFragment : Fragment() {
             findNavController().navigate(R.id.action_title_to_spotList)
         }
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == 1000) {
+            // 使用が許可された
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("debug", "checkSelfPermission true")
+
+            }
+        }
     }
 
 }
