b7735a76699fbcd4f09c2f0987841fb8ac5cf45f
nanairoaisu <b1018078@fun.ac.jp>
2020-10-02 10:40:23 +0900

    fix:SpotListとSpotRegister，title画面のlayout修正

--- Michishirube/app/src/main/java/com/example/michishirube/ui/title/TitleFragment.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/title/TitleFragment.kt
@@ -19,21 +19,6 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_title, container, false)
 
-//        val requestPermissionLauncher =
-//            registerForActivityResult(RequestPermission()
-//            ) { isGranted: Boolean ->
-//                if (isGranted) {
-//                    // Permission is granted. Continue the action or workflow in your
-//                    // app.
-//                } else {
-//                    // Explain to the user that the feature is unavailable because the
-//                    // features requires a permission that the user has denied. At the
-//                    // same time, respect the user's decision. Don't link to system
-//                    // settings in an effort to convince the user to change their
-//                    // decision.
-//                }
-//            }
-
         //押されたら
         view.ibNavigation.setOnClickListener {
             findNavController().navigate(R.id.action_title_to_naviEmotionSelect)
