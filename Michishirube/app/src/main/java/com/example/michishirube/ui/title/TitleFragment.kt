284e2e45a29377d2efb5a81303f5f8afba314e8a
nanairoaisu <b1018078@fun.ac.jp>
2020-10-03 00:53:20 +0900

    fix:不要なimport文を削除

--- Michishirube/app/src/main/java/com/example/michishirube/ui/title/TitleFragment.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/title/TitleFragment.kt
@@ -1,21 +1,15 @@
 package com.example.michishirube.ui.title
 
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.michishirube.R
 import kotlinx.android.synthetic.main.fragment_title.view.*
-import java.util.jar.Manifest
 
 class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
