96f6453c3c8e0d362a1e43ef1a445634519a7a77
nanairoaisu <b1018078@fun.ac.jp>
2020-10-04 14:21:59 +0900

    fix:NaviTimeSelectViewModel内容をsharedViewModelの方に統一した

--- Michishirube/app/src/main/java/com/example/michishirube/ui/NavigationSharedViewModel.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/NavigationSharedViewModel.kt
@@ -1,8 +1,20 @@
 package com.example.michishirube.ui
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 class NavigationSharedViewModel: ViewModel() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallBack: LocationCallback
 
     //emotionSelect
     var emotion = 0
@@ -31,8 +43,11 @@ class NavigationSharedViewModel: ViewModel() {
     }
 
     fun setLocation(latitude: Double?, longitude: Double?) {
-        deviceLatitude = latitude!!
-        deviceLongitude = longitude!!
+        if(latitude != null && longitude != null){
+            deviceLatitude = latitude
+            deviceLongitude = longitude
+        }
+
     }
 
     fun loadDestination(){//もしかしたらここら辺はちゃんとそれらの（？）ViewModelで書くかも
@@ -48,6 +63,45 @@ class NavigationSharedViewModel: ViewModel() {
     //naviEvaluation 2ndスプリント
     fun postEvaluate(){//もしかしたらここら辺はちゃんとそれらの（？）ViewModelで書くかも
         //たぶんここでコルーチン
+    }
+
+    //位置情報取得
+    private fun checkRequestPermission(activity: Activity, context: Context){
+        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(activity,permissions,1000)
+        }
+    }
+
+    fun getDeviceLocation(activity: Activity, context: Context){
+        fusedLocationClient = FusedLocationProviderClient(context)
+        checkRequestPermission(activity,context)
+
+        //初回取得時は，最後の位置情報を利用
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if(location != null){
+                setLocation(location.latitude, location.longitude)
+            }else{
+                //位置情報の取得に失敗しました，的な何かを出したい
+                //UIに関わる部分なのでiOSやデザインと話し合いが必要だと思ったので保留
+            }
+        }
+
+        locationCallBack = object : LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+                if(locationResult != null){
+                    setLocation(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
+                }
+            }
+        }
+
+        val locationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(3000L)
+
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallBack, null)
+
     }
 
 }
--- Michishirube/app/src/main/java/com/example/michishirube/ui/naviTimeSelect/NaviTimeSelectFragment.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/naviTimeSelect/NaviTimeSelectFragment.kt
@@ -1,35 +1,25 @@
 package com.example.michishirube.ui.naviTimeSelect
 
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.michishirube.R
 import com.example.michishirube.databinding.FragmentNaviTimeSelectBinding
 import com.example.michishirube.ui.NavigationSharedViewModel
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import kotlin.math.log
 
 
 class NaviTimeSelectFragment : Fragment() {
     private val sharedViewModel: NavigationSharedViewModel by activityViewModels()
-    private val viewModel: NaviTimeSelectViewModel by viewModels()
     private lateinit var binding:FragmentNaviTimeSelectBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentNaviTimeSelectBinding.inflate(inflater, container, false)
-        viewModel.getDeviceLocation(requireActivity(), requireContext())
+        sharedViewModel.getDeviceLocation(requireActivity(), requireContext())
 
         //Viewの設定
         binding.timePicker.setIs24HourView(true)
@@ -49,7 +39,6 @@ class NaviTimeSelectFragment : Fragment() {
 
         //決定ボタンを押下
         binding.btTimeDecision.setOnClickListener {
-            sharedViewModel.setLocation(viewModel.lat, viewModel.lon)
             //サーバとのやりとりが多分ここに入る
             findNavController().navigate(R.id.action_naviTimeSelect_to_naviDestination)
         }
