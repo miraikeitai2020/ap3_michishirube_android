8dddf41955893656f5e6a39658011c8743cce51d
nanairoaisu <b1018078@fun.ac.jp>
2020-10-13 22:07:21 +0900

    feature/graphQLを用いて目的地を持ってくる

--- Michishirube/app/src/main/java/com/example/michishirube/data/Repository.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/data/Repository.kt
@@ -1,9 +1,67 @@
 package com.example.michishirube.data
 
+import DestinationListQuery
+import android.content.Context
+import android.util.Log
+import android.widget.ArrayAdapter
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+
 class Repository {
-    //ここら辺でApolloClientを宣言して
-    //
-    suspend fun inputDestination(){
-        //ここら辺でクエリ飛ばす処理書くのかなぁ
-    }
+//
+//    //ここら辺でApolloClientを宣言して
+//    private fun apolloClient(context: Context): ApolloClient {
+//        return ApolloClient.builder()
+//            .serverUrl("https://miraikeitai2020-bff.herokuapp.com/query")
+//            .okHttpClient (
+//                OkHttpClient.Builder()
+//                    .addInterceptor(AuthorizationInterceptor(context))
+//                    .build()
+//            )
+//            .build()
+//    }
+//
+//    fun inputDestination(context: Context, latitude: Double, longitude: Double, worktime: Int, emotion: Int){
+//        CoroutineScope(Dispatchers.Default).launch {
+//            val res = try {
+//                apolloClient(context).query(
+//                    DestinationListQuery(
+//                        deviceLatitude = latitude,
+//                        deviceLongitude = longitude,
+//                        worktime = worktime,
+//                        emotion = emotion
+//                    )
+//                ).toDeferred().await()
+//            }catch (ex: ApolloException){
+//                Log.e("checker",ex.toString())
+//                return@launch
+//            }
+//            val destinationsName = res?.data?.spots?.spots?.map{ it?.name }?:return@launch
+//            withContext(Dispatchers.Main){
+//                val randomDestinationName = destinationsName.shuffled().first()
+//            }
+//
+//        }
+//    }
+//
+//    private class AuthorizationInterceptor(val context: Context): Interceptor{
+//        override fun intercept(chain: Interceptor.Chain): Response {
+//            val request = chain.request().newBuilder()
+////                .addHeader("id","nanairoaisu")
+////                .addHeader("pass","nanachan")
+//                .addHeader("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIyMDIwLTEwLTEyVDE3OjA1OjAyLjY1MDA4NDY4M1oiLCJzdWIiOiJuYW5haXNvYWlzdSJ9.Cjp90e5PJmWbqWBwXDAf2HNYdvSwEb69INNggX0tOHg")
+//                .build()
+//
+//            return chain.proceed(request)
+//        }
+//    }
+
 }
--- Michishirube/app/src/main/java/com/example/michishirube/ui/NavigationSharedViewModel.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/NavigationSharedViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.michishirube.ui
 
+import DestinationListQuery
 import android.Manifest
 import android.app.Activity
 import android.content.Context
@@ -8,11 +9,23 @@ import android.content.IntentSender
 import android.content.pm.PackageManager
 import android.location.Location
 import android.net.Uri
+import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 
 class NavigationSharedViewModel: ViewModel() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -31,12 +44,12 @@ class NavigationSharedViewModel: ViewModel() {
     var deviceLongitude = 0.0
 
     //spot何たらはgraphQlから値をとってくる
-    var spotName = ""
+//    var spotName: String? = ""
+    var spotName: MutableLiveData<String> = MutableLiveData<String>("")
 
     //目的地の緯度経度（とりあえず今は未来大が入っている）
-    var spotLatitude = 41.841714
-    var spotLongitude = 140.766817
-
+    var spotLatitude: Double? = 41.841714
+    var spotLongitude: Double? = 140.766817
 
     //emotionSelect
     fun setEmotionType(selectedEmotion: Int){
@@ -50,18 +63,43 @@ class NavigationSharedViewModel: ViewModel() {
         sumMinute = hour * 60 + minute
     }
 
-    fun setLocation(latitude: Double?, longitude: Double?) {
+    fun setDeviceLocation(latitude: Double?, longitude: Double?) {
         if(latitude != null && longitude != null){
             deviceLatitude = latitude
             deviceLongitude = longitude
         }
-
     }
 
-    fun loadDestination(){//もしかしたらここら辺はちゃんとそれらの（？）ViewModelで書くかも
+    fun loadDestination(context: Context){//もしかしたらここら辺はちゃんとそれらの（？）ViewModelで書くかも
         //Coroutinesを使用して，Repositryの関数を使って，目的地名を持ってくる
         //withContextでここの目的地名のテキスト（spotName）に値入れて，Fragmentの方でFragmentの方の目的地名（レイアウトと直結してる方）に値追加かな
         //上にプラスでgraphQLから値を撮ってきてspotLatitude,spotLongitudeに目的地の緯度経度をいれる
+        CoroutineScope(Dispatchers.IO).launch {
+            val res = try {
+                apolloClient(context).query(
+                    DestinationListQuery(
+                        deviceLatitude = deviceLatitude,
+                        deviceLongitude = deviceLongitude,
+                        worktime = sumMinute,
+                        emotion = emotion
+                    )
+                ).toDeferred().await()
+            }catch (ex: ApolloException){
+                Log.e("checker",ex.toString())
+                return@launch
+            }
+            val destinationsName = res?.data?.spots?.spots?.map{ it?.name }?:return@launch
+            val destinationsLatitude = res?.data?.spots?.spots?.map{ it?.locate?.latitude }?:return@launch
+            val destinationsLongitude = res?.data?.spots?.spots?.map{ it?.locate?.longitude }?:return@launch
+
+            withContext(Dispatchers.Main) {
+                val spotRandom = (0..destinationsName.size - 1).shuffled().first()
+                spotName.postValue(destinationsName[spotRandom])
+                spotLatitude = destinationsLatitude[spotRandom]
+                spotLongitude = destinationsLongitude[spotRandom]
+
+            }
+        }
     }
 
     //naviDestination
@@ -92,7 +130,7 @@ class NavigationSharedViewModel: ViewModel() {
         //初回取得時は，最後の位置情報を利用
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
             if(location != null){
-                setLocation(location.latitude, location.longitude)
+                setDeviceLocation(location.latitude, location.longitude)
             }else{
                 //位置情報の取得に失敗しました，的な何かを出したい
                 //UIに関わる部分なのでiOSやデザインと話し合いが必要だと思ったので保留
@@ -103,7 +141,7 @@ class NavigationSharedViewModel: ViewModel() {
             override fun onLocationResult(locationResult: LocationResult?) {
                 super.onLocationResult(locationResult)
                 if(locationResult != null){
-                    setLocation(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
+                    setDeviceLocation(locationResult.lastLocation.latitude, locationResult.lastLocation.longitude)
                 }
             }
         }
@@ -140,6 +178,30 @@ class NavigationSharedViewModel: ViewModel() {
                     // Ignore the error.
                 }
             }
+        }
+    }
+
+    //apollo
+    private fun apolloClient(context: Context): ApolloClient {
+        return ApolloClient.builder()
+            .serverUrl("https://miraikeitai2020-bff.herokuapp.com/query")
+            .okHttpClient (
+                OkHttpClient.Builder()
+                    .addInterceptor(AuthorizationInterceptor(context))
+                    .build()
+            )
+            .build()
+    }
+
+    private class AuthorizationInterceptor(val context: Context): Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request().newBuilder()
+//                .addHeader("id","nanairoaisu")
+//                .addHeader("pass","nanachan")
+                .addHeader("token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOiIyMDIwLTEwLTEyVDE3OjA1OjAyLjY1MDA4NDY4M1oiLCJzdWIiOiJuYW5haXNvYWlzdSJ9.Cjp90e5PJmWbqWBwXDAf2HNYdvSwEb69INNggX0tOHg")
+                .build()
+
+            return chain.proceed(request)
         }
     }
 
