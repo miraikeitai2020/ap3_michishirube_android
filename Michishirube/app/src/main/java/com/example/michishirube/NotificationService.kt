712d334a1aa8361b98f0626bbfadc42e61286ba5
nanairoaisu <b1018078@fun.ac.jp>
2020-11-04 04:48:33 +0900

    feat:GoogleMap遷移後に通知が出るようにした

---
+++ Michishirube/app/src/main/java/com/example/michishirube/NotificationService.kt
@@ -0,0 +1,54 @@
+package com.example.michishirube
+
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.VISIBILITY_PUBLIC
+import androidx.core.app.NotificationManagerCompat
+import com.example.michishirube.ui.MainActivity
+import com.example.michishirube.ui.naviEvaluation.NaviEvaluationFragment
+
+class NotificationService : Service() {
+    private var startMode: Int = 0
+    private var binder: IBinder? = null
+    private var allowRebind: Boolean = false
+
+    override fun onCreate() {
+        super.onCreate()
+        val id = getString(R.string.channel_id)
+        val name = getString(R.string.channel_name)
+        val descriptionText = getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(id, name, importance)
+        channel.description = descriptionText
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(applicationContext, 0, intent, 0)
+
+        val notification = NotificationCompat.Builder(this, id)
+            .setContentTitle(getString(R.string.mtg_notification_title_destination))
+            .setContentText(getString(R.string.mtg_notification_text_destination))
+            .setSmallIcon(R.drawable.icon)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        startForeground(1,notification)
+
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
--- Michishirube/app/src/main/java/com/example/michishirube/ui/naviDestination/NaviDestinationFragment.kt
+++ Michishirube/app/src/main/java/com/example/michishirube/ui/naviDestination/NaviDestinationFragment.kt
@@ -1,13 +1,16 @@
 package com.example.michishirube.ui.naviDestination
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startForegroundService
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.michishirube.NotificationService
 import com.example.michishirube.R
 import com.example.michishirube.databinding.FragmentNaviDestinationBinding
 import com.example.michishirube.ui.NavigationSharedViewModel
@@ -35,6 +38,9 @@ class NaviDestinationFragment : Fragment() {
         binding.ibGo.setOnClickListener{
             startActivity(sharedViewModel.intentDestination())
             findNavController().navigate(R.id.action_naviDestination_to_naviNavigating)
+            val serviceIntent = Intent(activity, NotificationService::class.java)
+            activity?.startForegroundService(serviceIntent)
+
         }
 
 //        binding.btReload.setOnClickListener {
